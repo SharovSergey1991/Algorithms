63cf248e71e0e765f9353d23f4ed03d1d452e2f6
Sharov Sergey <sharovsergey1991@gmail.com>
2021-12-06 22:04:02 +1000

    Домашнее задание №1

---
+++ Algorithms/Algorithms/Fibonachi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Fibonachi
+    {
+        public static int FibRecurs (int n) // Рекурсивная реализация расчета числа Фибоначи
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+
+            return FibRecurs(n - 1) + FibRecurs(n - 2);
+        }
+
+        public static int FibCycle (int n) // Реализация расчета числа Фибоначи через цикл
+        {
+            int fibNum1 = 1;
+            int fibNum2 = 1;
+            int fibSum = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i == 1)
+                {
+                    continue;
+                }
+                else
+                {
+                    fibSum = fibNum1 + fibNum2;
+                    fibNum1 = fibNum2;
+                    fibNum2 = fibSum;
+                }
+            }
+            return fibSum;
+        }
+    }
+}
---
+++ Algorithms/Algorithms/Numbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Numbers
+    {
+        static public bool IsSimple(int number)
+        {
+            int d = 0;
+            int i = 2;
+            while (i < number)
+            {
+                if (number % i == 0) d++;
+                i++;
+            }
+            if (d == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
